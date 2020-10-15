a93fbdd66f6a6260dc3098375bc7c32233f1575e
baijz <baijz@winning.com.cn>
2020-10-15 10:48:51 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,5 @@
+1.安装所需插件
+路由
+yarn add react-router-dom axios less-loader
+ui 组件
+ant-D
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Life from './demo/Life'
+function App() {
+  return (
+    <div className="App">
+      <Life></Life>
+    </div>
+  );
+}
+
+export default App;
