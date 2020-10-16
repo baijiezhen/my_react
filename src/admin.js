954e62dc475b497d02127df6d6a0916bf8662238
baijz <baijz@winning.com.cn>
2020-10-16 15:55:11 +0800

    左侧菜单简单实现

--- src/admin.js
+++ src/admin.js
@@ -3,14 +3,18 @@ import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+
+
+// import './index.less'
 import './style/common.less'
+
 export default class Admin extends React.Component{
     render(){
         return <Row className="container">
                  <Col span={4} className="nav-left"><NavLeft></NavLeft></Col>
                  <Col span={20} className="main">
                     <Header></Header>
-                    <Row className="content"></Row>   
+                <Row className="content"></Row>   
                     <Footer></Footer>
        </Col>
       </Row>
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import menuConfig from '../../config/menuConfig'
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu, Icon } from 'antd';
+import  menuConfig   from './../../config/menuConfig.js'
 import './index.less'
 const { SubMenu } = Menu;
+
+console.log(menuConfig)
 export default class NavLeft extends React.Component{
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(menuConfig);
+
+        this.setState({
+            menuTreeNode
+        })
+    }
+    //  菜单渲染
+    renderMenu =(data)=>{
+        return data.map((item) => {
+            console.log(item)
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>
+                {item.title}
+                {/* <NavLink to={item.key}>{item.title}</NavLink> */}
+            </Menu.Item>
+        })
+    }
     render(){
         return <div>
             <div className="logo">
@@ -12,12 +37,28 @@ export default class NavLeft extends React.Component{
                         <h1>Imooc MS</h1>
             </div>
             <Menu theme="dark" mode="vertical">
-                <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                {/* <SubMenu key="sub1" title={
+        <span>
+          <Icon type="mail" />
+          <span>Navigation One</span>
+        </span>
+      } >
                   <Menu.Item key="1">Option 1</Menu.Item>
                   <Menu.Item key="2">Option 2</Menu.Item>
                   <Menu.Item key="3">Option 3</Menu.Item>
                   <Menu.Item key="4">Option 4</Menu.Item>
                 </SubMenu>
+                <SubMenu key="sub2" title="Navigation Two">
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3"  title="Navigation Three">
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.Item key="11">Option 11</Menu.Item>
+            <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu> */}
+                 { this.state.menuTreeNode }
             </Menu>
         </div>
     }
