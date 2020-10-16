3ee28382a62dd818885240ce8579600826cab8df
baijz <baijz@winning.com.cn>
2020-10-16 16:31:44 +0800

    jsonp请求百度api

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -3,8 +3,6 @@ import { Menu, Icon } from 'antd';
 import  menuConfig   from './../../config/menuConfig.js'
 import './index.less'
 const { SubMenu } = Menu;
-
-console.log(menuConfig)
 export default class NavLeft extends React.Component{
     componentWillMount() {
         const menuTreeNode = this.renderMenu(menuConfig);
@@ -37,27 +35,6 @@ export default class NavLeft extends React.Component{
                         <h1>Imooc MS</h1>
             </div>
             <Menu theme="dark" mode="vertical">
-                {/* <SubMenu key="sub1" title={
-        <span>
-          <Icon type="mail" />
-          <span>Navigation One</span>
-        </span>
-      } >
-                  <Menu.Item key="1">Option 1</Menu.Item>
-                  <Menu.Item key="2">Option 2</Menu.Item>
-                  <Menu.Item key="3">Option 3</Menu.Item>
-                  <Menu.Item key="4">Option 4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" title="Navigation Two">
-                <Menu.Item key="5">Option 5</Menu.Item>
-                <Menu.Item key="6">Option 6</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3"  title="Navigation Three">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu> */}
                  { this.state.menuTreeNode }
             </Menu>
         </div>
